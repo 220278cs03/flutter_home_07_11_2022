6adc751f30cdc7f7d657387fb2a3cdfa28a25fd3
220278cs03 <220278@akfauniversity.org>
2022-11-08 17:45:49 +0500

    uyishi_1

---
+++ lib/main.dart
@@ -0,0 +1,584 @@
+// 1 - topshiriq
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Container(
+//         color: Colors.white,
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Container(
+//               height: 200,
+//               width: 50,
+//               color: Colors.blue,
+//               child: const Center(
+//                 child: Text(
+//                   "F",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 200,
+//               width: 50,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue.withOpacity(0.7),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "L",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 200,
+//               width: 50,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue.withOpacity(0.4),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "U",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 200,
+//               width: 50,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue.withOpacity(0.1),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "T",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 200,
+//               width: 50,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue.withOpacity(0.4),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "T",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 200,
+//               width: 50,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue.withOpacity(0.6),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "E",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 200,
+//               width: 50,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue,
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "R",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+// 2- topshiriq
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Container(
+//         color: Colors.white,
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Container(
+//               height: 100,
+//               width: 500,
+//               color: Colors.blue,
+//               child: const Center(
+//                 child: Text(
+//                   "F",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 100,
+//               width: 500,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue.withOpacity(0.7),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "L",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 100,
+//               width: 500,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue.withOpacity(0.4),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "U",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 100,
+//               width: 500,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue.withOpacity(0.1),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "T",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 100,
+//               width: 500,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue.withOpacity(0.4),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "T",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 100,
+//               width: 500,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue.withOpacity(0.6),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "E",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 100,
+//               width: 500,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue,
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "R",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+// 3 - topshiriq
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Container(
+//         color: Colors.white,
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Container(
+//               height: 100,
+//               width: 500,
+//               color: Colors.pink,
+//               child: const Center(
+//                 child: Text(
+//                   "G",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 100,
+//               width: 500,
+//               decoration: BoxDecoration(
+//                 color: Colors.pink.withOpacity(0.7),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "U",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 100,
+//               width: 500,
+//               decoration: BoxDecoration(
+//                 color: Colors.pink.withOpacity(0.4),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "L",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 100,
+//               width: 500,
+//               decoration: BoxDecoration(
+//                 color: Colors.pink.withOpacity(0.1),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "N",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 100,
+//               width: 500,
+//               decoration: BoxDecoration(
+//                 color: Colors.pink.withOpacity(0.4),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "O",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 100,
+//               width: 500,
+//               decoration: BoxDecoration(
+//                 color: Colors.pink.withOpacity(0.6),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "Z",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 100,
+//               width: 500,
+//               decoration: BoxDecoration(
+//                 color: Colors.pink,
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "A",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+// 4 - topshiriq
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Container(
+//         color: Colors.white,
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Container(
+//               height: 200,
+//               width: 50,
+//               color: Colors.pink,
+//               child: const Center(
+//                 child: Text(
+//                   "G",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 200,
+//               width: 50,
+//               decoration: BoxDecoration(
+//                 color: Colors.pink.withOpacity(0.7),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "U",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 200,
+//               width: 50,
+//               decoration: BoxDecoration(
+//                 color: Colors.pink.withOpacity(0.4),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "L",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 200,
+//               width: 50,
+//               decoration: BoxDecoration(
+//                 color: Colors.pink.withOpacity(0.1),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "N",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 200,
+//               width: 50,
+//               decoration: BoxDecoration(
+//                 color: Colors.pink.withOpacity(0.4),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "O",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 200,
+//               width: 50,
+//               decoration: BoxDecoration(
+//                 color: Colors.pink.withOpacity(0.6),
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "Z",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 200,
+//               width: 50,
+//               decoration: BoxDecoration(
+//                 color: Colors.pink,
+//               ),
+//               child: const Center(
+//                 child: Text(
+//                   "A",
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+// 5 - topshiriq
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.green,
+                ),
+                Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.green,
+                ),
+            ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.green,
+                ),
+                Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.green,
+                ),
+                Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+          ],
+        ),
+
+    ),
+    );
+  }
+}
